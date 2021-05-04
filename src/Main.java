8052473af5e1dc52faa26767ea6540ffaadb2e9d
Hikmet Göksu Yılmaz <46975324+goksu999@users.noreply.github.com>
2021-05-04 21:28:55 +0300

    Update Main.java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package interfaceAbstractDemo;
+
+import java.util.GregorianCalendar;
+
+import Abstract.BaseCustomerManager;
+import adapters.MernisServiceAdapter;
+import concrete.NeroCustomerManager;
+import concrete.StarbucksCustomerManager;
+import entities.Customer;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		BaseCustomerManager customerManager = new NeroCustomerManager();
+		 customerManager.save(new Customer(1, "Göksu", "Yılmaz", new
+		 GregorianCalendar(1993, 9 , 01).getTime(),"11111111111"));
+		System.out.println(customerManager);
+	}
+
+}
